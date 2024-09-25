1b145464891500a11a67fbab2da8d6bb5aa390f9
kali <kali@kali>
2024-09-25 21:16:57 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,102 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173", 
+  },
+});
+
+const allUsers = {};
+const allRooms = [];
+
+io.on("connection", (socket) => {
+  allUsers[socket.id] = {
+    socket: socket,
+    online: true,
+    playing: false, 
+  };
+
+  socket.on("request_to_play", (data) => {
+    const currentUser = allUsers[socket.id];
+    currentUser.playerName = data.playerName;
+    currentUser.playing = false;
+
+    let opponentPlayer;
+
+    // Find an available opponent who is online and not playing
+    for (const key in allUsers) {
+      const user = allUsers[key];
+      if (user.online && !user.playing && socket.id !== key) {
+        opponentPlayer = user;
+        break;
+      }
+    }
+
+    if (opponentPlayer) {
+      // Mark both players as playing
+      currentUser.playing = true;
+      opponentPlayer.playing = true;
+
+      // Add players to a new room
+      allRooms.push({
+        player1: opponentPlayer,
+        player2: currentUser,
+      });
+
+      // Notify both players that an opponent was found
+      currentUser.socket.emit("OpponentFound", {
+        opponentName: opponentPlayer.playerName,
+        playingAs: "circle",
+      });
+
+      opponentPlayer.socket.emit("OpponentFound", {
+        opponentName: currentUser.playerName,
+        playingAs: "cross",
+      });
+
+      // Handle moves
+      currentUser.socket.on("playerMoveFromClient", (data) => {
+        opponentPlayer.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+
+      opponentPlayer.socket.on("playerMoveFromClient", (data) => {
+        currentUser.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+    } else {
+      currentUser.socket.emit("OpponentNotFound");
+    }
+  });
+
+  socket.on("disconnect", function () {
+    const currentUser = allUsers[socket.id];
+    if (currentUser) {
+      currentUser.online = false;
+      currentUser.playing = false;
+
+      // Notify opponent if disconnect occurs during a match
+      for (let index = 0; index < allRooms.length; index++) {
+        const { player1, player2 } = allRooms[index];
+
+        if (player1.socket.id === socket.id) {
+          player2.socket.emit("opponentLeftMatch");
+          break;
+        }
+
+        if (player2.socket.id === socket.id) {
+          player1.socket.emit("opponentLeftMatch");
+          break;
+        }
+      }
+    }
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
